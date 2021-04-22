08287eaaf4429cfc4b88678fbb4e1caa2e2cc5f3
Ayush Chatpalliwar <ayushchatpalliwar98@gmail.com>
2021-04-22 19:37:05 +0530

    First Commit

---
+++ EmployeeMicroservices/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeMicroservices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        //[Route("api/[controller]")]
+        public IActionResult GetEmployee()
+        {
+            return Ok();
+        }
+
+    }
+}
